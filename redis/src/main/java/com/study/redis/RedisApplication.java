95bbef852bbae89022d3f52ebf69cf0da04aa886
IMWoo94 <leemeawoo94@gmail.com>
2024-03-22 18:25:41 +0900

    feat : redis client String 접근
    1. Jedis 를 통해 java 기반의 레디스 클라이언트 사용
    2. connection pool 을 통해 connection 가져오기
    3. String 데이터 타입 등록 및 가져오기

--- redis/src/main/java/com/study/redis/RedisApplication.java
+++ redis/src/main/java/com/study/redis/RedisApplication.java
@@ -1,13 +1,34 @@
 package com.study.redis;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 @SpringBootApplication
 public class RedisApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RedisApplication.class, args);
+
+		try(var jedisPool = new JedisPool("127.0.0.1", 6379);){
+			try(Jedis jedis = jedisPool.getResource()){
+				jedis.set("users:300:name", "lee");
+				jedis.set("users:300:age", "20");
+				jedis.set("users:300:email", "lee@gmail.com");
+
+				String s = jedis.get("users:300:email");
+				System.out.println(s);
+
+				List<String> mget = jedis.mget("users:300:name", "users:300:age", "users:300:email");
+				mget.forEach(System.out::println);
+
+
+			}
+		}
 	}
 
 }
