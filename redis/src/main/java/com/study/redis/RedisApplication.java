f5904c65ed4d09f6e0e6107e8aa0233afb171cee
IMWoo94 <leemeawoo94@gmail.com>
2024-03-23 16:25:41 +0900

    refactor : redis String type 클래스 수정
    1. JedisPool 필드의 경우 외부에서 주입 받아서 사용하도록 변경 - 지속적으로 type 마다 반복되는 사항

--- redis/src/main/java/com/study/redis/RedisApplication.java
+++ redis/src/main/java/com/study/redis/RedisApplication.java
@@ -1,7 +1,5 @@
 package com.study.redis;
 
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,7 +7,6 @@ import com.study.redis.type.RedisStringType;
 
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.Pipeline;
 
 @SpringBootApplication
 public class RedisApplication {
@@ -17,7 +14,19 @@ public class RedisApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RedisApplication.class, args);
 
-		new RedisStringType();
+		try (JedisPool jedisPool = new JedisPool("127.0.0.1", 6379);) {
+			try (Jedis jedis = jedisPool.getResource();) {
+
+				// redis String type
+				RedisStringType redisStringType = new RedisStringType(jedis);
+				redisStringType.run();
+
+				// redis List ( stack, queue, block )
+				// new RedisListAndSetType(jedis);
+			}
+
+		}
+
 	}
 
 }
--- redis/src/main/java/com/study/redis/type/RedisStringType.java
+++ redis/src/main/java/com/study/redis/type/RedisStringType.java
@@ -2,41 +2,54 @@ package com.study.redis.type;
 
 import java.util.List;
 
+import lombok.extern.slf4j.Slf4j;
 import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.Pipeline;
 
+@Slf4j
 public class RedisStringType {
 
-	public RedisStringType() {
-		try(var jedisPool = new JedisPool("127.0.0.1", 6379);){
-			try(Jedis jedis = jedisPool.getResource()){
-				Pipeline pipelined = jedis.pipelined();
-				pipelined.set("users:400:email", "greg@gmail.com");
-				pipelined.set("users:400:name", "greg");
-				pipelined.set("users:400:age", "20");
-				List<Object> objects = pipelined.syncAndReturnAll();
-				objects.forEach(System.out::println);
-				// 현재 상태에서는 set, get 이 일어날떄 한번 한번 일어나게 된다.
-				// 일괄 처리 할 수 있는 Pipelining 을 적용하자
-				jedis.set("users:300:name", "lee");
-				jedis.set("users:300:age", "20");
-				jedis.set("users:300:email", "lee@gmail.com");
+	private final Jedis jedis;
 
-				String s = jedis.get("users:300:email");
-				System.out.println(s);
+	public RedisStringType(Jedis jedis) {
+		this.jedis = jedis;
+	}
 
-				List<String> mget = jedis.mget("users:300:name", "users:300:age", "users:300:email");
-				mget.forEach(System.out::println);
+	public void run() {
+		Pipeline pipelined = jedis.pipelined();
+		pipelined.set("users:400:email", "greg@gmail.com");
+		pipelined.set("users:400:name", "greg");
+		pipelined.set("users:400:age", "20");
+		List<Object> objects = pipelined.syncAndReturnAll();
+		objects.forEach(it -> log.info(it.toString()));
+		// 현재 상태에서는 set, get 이 일어날떄 한번 한번 일어나게 된다.
+		// 일괄 처리 할 수 있는 Pipelining 을 적용하자
+		jedis.set("users:300:name", "lee");
+		jedis.set("users:300:age", "20");
+		jedis.set("users:300:email", "lee@gmail.com");
 
-				long counter1 = jedis.incr("counter");
-				System.out.println(counter1);
-				long counter2 = jedis.incrBy("counter", 20);
-				System.out.println(counter2);
+		String s = jedis.get("users:300:email");
+		log.info(s);
 
-				long decr = jedis.decr("counter");
-				System.out.println(decr);
-			}
-		}
+		List<String> mget = jedis.mget("users:300:name", "users:300:age", "users:300:email");
+		mget.forEach(log::info);
+
+		long counter1 = jedis.incr("counter");
+		log.info("jedis.incr(\"counter\") {}", counter1);
+		long counter2 = jedis.incrBy("counter", 20);
+		log.info("jedis.incrBy(\"counter\", 20) {}", counter2);
+
+		long decr = jedis.decr("counter");
+		log.info("jedis.decr(\"counter\") {}", decr);
+
+		Pipeline removePipeline = jedis.pipelined();
+		removePipeline.unlink("users:400:email");
+		removePipeline.unlink("users:400:name");
+		removePipeline.unlink("users:400:age");
+		removePipeline.unlink("users:300:email");
+		removePipeline.unlink("users:300:name");
+		removePipeline.unlink("users:300:age");
+		removePipeline.unlink("counter");
+		removePipeline.syncAndReturnAll();
 	}
 }
