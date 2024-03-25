e32e0ad13402c0f4a9e184604785b7782282e2f3
IMWoo94 <leemeawoo94@gmail.com>
2024-03-25 16:38:52 +0900

    init : Redis Geospatial type 을 활용한 주변 카페 찾기 프로젝트 준비

---
+++ CafeAroundMe/src/main/java/com/finder/CafeAroundMe/CafeAroundMeApplication.java
@@ -0,0 +1,13 @@
+package com.finder.CafeAroundMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeAroundMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeAroundMeApplication.class, args);
+	}
+
+}
---
+++ CafeAroundMe/src/test/java/com/finder/CafeAroundMe/CafeAroundMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finder.CafeAroundMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeAroundMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
