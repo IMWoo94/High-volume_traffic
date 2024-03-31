504a0ad54e9f0ec7d85d864c44d87c59da9fb6de
IMWoo94 <leemeawoo94@gmail.com>
2024-03-31 18:29:39 +0900

    refactor : 응답 responseEntity 수정

--- redis/src/main/java/com/study/redis/performance/controller/RedisSearchPerformanceController.java
+++ redis/src/main/java/com/study/redis/performance/controller/RedisSearchPerformanceController.java
@@ -1,10 +1,14 @@
 package com.study.redis.performance.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +28,7 @@ public class RedisSearchPerformanceController {
 	private final Jedis jedis;
 
 	@GetMapping("/keys")
-	public void commendKeys() {
+	public ResponseEntity commendKeys() {
 		long start = System.currentTimeMillis();
 
 		log.info("keys 명령어로 대량 key 정보를 가져오는 경우 싱글 쓰레드 기반으로 지연이 발생 할 수 있다.");
@@ -33,10 +37,15 @@ public class RedisSearchPerformanceController {
 		long count = keys.size();
 		long end = System.currentTimeMillis();
 		log.info("total : {}, time : {}", count, end - start);
+
+		Map<String, Object> body = new HashMap<>();
+		body.put("total", count);
+		body.put("time", end - start);
+		return new ResponseEntity(body, HttpStatus.OK);
 	}
 
 	@GetMapping("/scan")
-	public void commendScan() {
+	public ResponseEntity commendScan() {
 
 		log.info("scan 명령어로 cursor 형식으로 갯수를 지정해서 가져오면 성능 문제를 개선할 수 있다.");
 		ScanResult<String> scanResult;
@@ -59,6 +68,11 @@ public class RedisSearchPerformanceController {
 		long count = keys.size();
 		double avg = times.stream().mapToLong(Long::longValue).average().getAsDouble();
 		log.info("total : {}, time : {}", count, avg);
+
+		Map<String, Object> body = new HashMap<>();
+		body.put("total", count);
+		body.put("time", avg);
+		return new ResponseEntity(body, HttpStatus.OK);
 	}
 
 }
